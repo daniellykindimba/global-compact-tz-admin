f9bbb5c61f9e0d05b03fdb91f8fac02f5deb522d
Walter Kimaro <wakyj07@gmail.com>
2019-07-31 08:16:44 +0300

    Set max width for login form

--- src/UI/pages/Login.vue
+++ src/UI/pages/Login.vue
@@ -4,7 +4,7 @@
       <v-content>
         <v-container fluid fill-height>
           <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md4>
+            <v-flex xs12 style="max-width: 400px;">
               <v-card class="elevation-12">
                 <v-toolbar dark color="primary">
                   <v-toolbar-title>Staff Login</v-toolbar-title>
